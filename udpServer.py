3e9c15e984ba39dd63cfd9b41da5e0af9f3c3962
AshtonDavis2003 <ashtond@uark.edu>
2025-04-15 18:09:46 -0500

    Update udpServer.py

--- udpServer.py
+++ udpServer.py
@@ -1,8 +1,8 @@
 import socket
 import config
-from playerAction import handle_score_event, player_scores
 
 def run_server(score_labels=None, player_frames=None):
+    from playerAction import handle_score_event, player_scores
     localIP = config.NETWORK_ADDRESS
     localPort = 7501  # Receiving port
     bufferSize = 1024
