36e43d3798a0cd5ceea4be2c1aba5c1c87935eed
McKaylaLemon <mjlemon@uark.edu>
2025-04-15 00:51:37 -0500

    Update randomMusic.py
    Added code for randomly picking the tracks

--- randomMusic.py
+++ randomMusic.py
@@ -1,2 +1,27 @@
 from pydub import AudioSegment
+from pydub.playback import _play_with_simpleaudio
 import gametrack as gt
+import random
+import time
+
+tracks = [
+    "Track1.mp3",
+    "Track2.mp3",
+    "Track3.mp3",
+    "Track4.mp3",
+    "Track5.mp3",
+    "Track6.mp3",
+    "Track7.mp3",
+    "Track8.mp3"
+]
+
+# Randomly select one track
+selected_track = random.choice(tracks)
+print(f"Now playing: {selected_track}")
+
+# Load and play the audio
+audio = AudioSegment.from_file(selected_track)
+play_obj = _play_with_simpleaudio(audio)
+
+# Wait until playback is finished
+play_obj.wait_done()
