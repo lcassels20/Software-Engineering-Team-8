44adb6e5ed7b6e151e62f5d42ebe2105b3cd007d
AshtonDavis2003 <ashtond@uark.edu>
2025-04-15 21:56:46 -0500

    Update udpClient.py

--- udpClient.py
+++ udpClient.py
@@ -1,18 +1,23 @@
 import socket
+import config
 
-msgFromClient       = "Hello UDP Server"
-bytesToSend         = str.encode(msgFromClient)
-serverAddressPort   = ("127.0.0.1", 7501)
-bufferSize          = 1024
+def run_client():
+    message = b"Hello UDP Server"
+    serverAddressPort = (config.NETWORK_ADDRESS, 7501)
+    bufferSize = 1024
 
-# Create a UDP socket at client side
-UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    try:
+        UDPClientSocket.sendto(message, serverAddressPort)
+        print("UDP Client sent: Hello UDP Server to", serverAddressPort)
+        msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+        print("Message from Server:", msgFromServer[0].decode())
+    except Exception as e:
+        print("UDP Client failed to communicate:", e)
+    finally:
+        UDPClientSocket.close()
 
-# Send to server using created UDP socket
-UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+if __name__ == "__main__":
+    run_client()
 
-msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-msg = "Message from Server {}".format(msgFromServer[0])
-
-print(msg)
 
