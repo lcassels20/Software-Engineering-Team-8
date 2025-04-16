ab3a9c0374f1982a2536f9e87a04be94038629b7
AshtonDavis2003 <ashtond@uark.edu>
2025-04-15 22:44:00 -0500

    Update trafficGenerator.py

--- trafficGenerator.py
+++ trafficGenerator.py
@@ -1,6 +1,7 @@
 import socket
 import random
 import time
+import select
 
 print("Select network address (default is 127.0.0.1)")
 print("Enter new network address or press enter for default:", end=' ')
@@ -36,12 +37,23 @@ for msg in startup_messages:
 
 # Wait for game to send "202"
 print("\nwaiting for start from game_software")
-while True:
-    data, addr = UDPServerSocketReceive.recvfrom(bufferSize)
-    message = data.decode().strip()
-    print("Received from game software:", message)
-    if message == "202":
-        break
+start_received = False
+UDPServerSocketReceive.setblocking(0)
+
+timeout = time.time() + 20  # 20 sec timeout
+while time.time() < timeout:
+    ready = select.select([UDPServerSocketReceive], [], [], 1)
+    if ready[0]:
+        data, addr = UDPServerSocketReceive.recvfrom(bufferSize)
+        message = data.decode().strip()
+        print("Received from game software:", message)
+        if message == "202":
+            start_received = True
+            break
+
+if not start_received:
+    print("Timeout waiting for game start.")
+    exit()
 
 # Now simulate traffic
 players = [red1, red2, green1, green2]
@@ -57,4 +69,5 @@ while True:
     received_data, address = UDPServerSocketReceive.recvfrom(bufferSize)
     print("received from game:", received_data.decode())
     time.sleep(2)
+
 
