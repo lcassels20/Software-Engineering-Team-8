176378c43445f0ec2b032ee5b95fc188667cd6d7
AshtonDavis2003 <ashtond@uark.edu>
2025-04-17 16:36:14 -0500

    Update trafficGenerator.py

--- trafficGenerator.py
+++ trafficGenerator.py
@@ -1,85 +1,81 @@
 import socket
 import random
 import time
+import config  # Use shared network address
 
-#Same as Professor's file provided, with added option to choose 
-#a new network address other than the default
-def get_network_address():
-	print("Select network address (default is 127.0.0.1)")
-	new_address = input("Enter new network address or press enter for default:")
-	return new_address.strip() if new_address else "127.0.0.1"
+bufferSize = 1024
+network_address = config.NETWORK_ADDRESS
+game_listen_port = 7501
 
-bufferSize  = 1024
-network_address = get_network_address()
-game_listen_port = 7501  # the game's listening port
-local_receive_port = 7500  # the generator's bound port for replies
+print("\nThis program will generate some test traffic for 2 players on the red")
+print("team as well as 2 players on the green team.\n")
 
-print('this program will generate some test traffic for 2 players on the red ')
-print('team as well as 2 players on the green team')
-print('')
+try:
+    red1    = str(int(input('Enter equipment ID of Red player 1 ==> ')))
+    red2    = str(int(input('Enter equipment ID of Red player 2 ==> ')))
+    green1  = str(int(input('Enter equipment ID of Green player 1 ==> ')))
+    green2  = str(int(input('Enter equipment ID of Green player 2 ==> ')))
+except ValueError:
+    print("All equipment IDs must be numeric.")
+    exit()
 
-red1 = input('Enter equipment id of red player 1 ==> ')
-red2 = input('Enter equipment id of red player 2 ==> ')
-green1 = input('Enter equipment id of green player 1 ==> ')
-green2 = input('Enter equipment id of green player 2 ==> ')
+# Single socket for all sending/receiving
+UDPSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+UDPSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+UDPSocket.settimeout(150)
+UDPSocket.bind((network_address, 0))  # Let OS assign port
 
-# Use a single socket for sending and receiving
-UDPSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-UDPSocket.settimeout(150)  # wait long enough for game start
-UDPSocket.bind((network_address, local_receive_port))
+print("Traffic generator bound on:", UDPSocket.getsockname())
+print("\nWaiting for start from game_software...")
 
-# wait for start from game software
-print("\nwaiting for start from game_software")
-
-start_signal_received = False
-while not start_signal_received:
-	try:
-		received_data, address = UDPSocket.recvfrom(bufferSize)
-		received_data = received_data.decode('utf-8')
-		print("Received from game software:", received_data)
-		if received_data == "202":
-			start_signal_received = True
-	except socket.timeout:
-		print("No response from game software (timed out). Exiting.")
-		exit()
-
-print('')
-
-# create events, random player and order
-counter = 0
-
+# Wait for 202 signal from game
 while True:
-	redplayer = random.choice([red1, red2])
-	greenplayer = random.choice([green1, green2])
+    try:
+        data, addr = UDPSocket.recvfrom(bufferSize)
+        msg = data.decode('utf-8')
+        print("Received from game software:", msg)
+        if msg == "202":
+            break
+    except socket.timeout:
+        print("Timed out waiting for game start.")
+        exit()
 
-	if random.randint(0, 1) == 0:
-		message = f"{redplayer}:{greenplayer}"
-	else:
-		message = f"{greenplayer}:{redplayer}"
+print("Starting traffic simulation...")
+time.sleep(0.5)  # Let server fully initialize
 
-	if counter == 10:
-		message = f"{redplayer}:43"
-	if counter == 20:
-		message = f"{greenplayer}:53"
+counter = 0
+while True:
+    redplayer = random.choice([red1, red2])
+    greenplayer = random.choice([green1, green2])
 
-	print("transmitting to game:", message)
-	UDPSocket.sendto(message.encode(), (network_address, game_listen_port))
+    if random.randint(0, 1) == 0:
+        message = f"{redplayer}:{greenplayer}"
+    else:
+        message = f"{greenplayer}:{redplayer}"
 
-	try:
-		received_data, address = UDPSocket.recvfrom(bufferSize)
-		received_data = received_data.decode('utf-8')
-		print("Received from game software:", received_data)
-	except socket.timeout:
-		print("No response from game software (timed out). Ending traffic.")
-		break
+    if counter == 10:
+        message = f"{redplayer}:43"
+    if counter == 20:
+        message = f"{greenplayer}:53"
 
-	print('')
-	counter += 1
-	if received_data == '221':
-		break
-	time.sleep(random.randint(1, 3))
+    print(f"\nTransmitting to game: {message}")
+    UDPSocket.sendto(message.encode(), (network_address, game_listen_port))
 
-print("program complete")
+    try:
+        response, addr = UDPSocket.recvfrom(bufferSize)
+        print("  ↳ Response from game:", response.decode())
+    except socket.timeout:
+        print("No response. Ending simulation.")
+        break
+
+    if response.decode().strip() == "221":
+        break
+
+    counter += 1
+    time.sleep(random.randint(1, 3))
+
+print("Traffic simulation complete.")
+
 
 
 
