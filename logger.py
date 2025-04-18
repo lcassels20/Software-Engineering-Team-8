df055d9d09fbc8a61903da8d085e3a9ba14fe0dc
AshtonDavis2003 <ashtond@uark.edu>
2025-04-17 20:13:18 -0500

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,10 +1,11 @@
-# logger.py
 event_log = None
 
 def log_event(message):
     global event_log
     if event_log:
-        event_log.config(state="normal")
-        event_log.insert("end", message + "\n")
-        event_log.see("end")
-        event_log.config(state="disabled")
+        def safe_insert():
+            event_log.config(state="normal")
+            event_log.insert("end", message + "\n")
+            event_log.see("end")
+            event_log.config(state="disabled")
+        event_log.after(0, safe_insert)  
